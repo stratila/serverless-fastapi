589eae962674576771d6e5fc381f716ae3c74a99
Bohdan Stratila <googlobody@gmail.com>
2023-06-13 02:14:15 +0300

    add s3 bucket

--- infra/main.tf
+++ infra/main.tf
@@ -18,3 +18,21 @@ terraform {
 provider "aws" {
   region = "eu-central-1"
 }
+resource "random_pet" "fast_api_lambda_bucket_name" {
+  prefix = "fastapi-lambda-bucket"
+  length = 2
+}
+
+resource "aws_s3_bucket" "fast_api_lambda_bucket" {
+  bucket        = random_pet.fast_api_lambda_bucket_name.id
+  force_destroy = true
+}
+
+resource "aws_s3_bucket_public_access_block" "fast_api_lambda_bucket" {
+  bucket = aws_s3_bucket.fast_api_lambda_bucket.id
+
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
