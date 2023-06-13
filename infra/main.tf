3899d72f9593aaa0848f368b4de23151d2416381
Bohdan Stratila <googlobody@gmail.com>
2023-06-13 11:32:37 +0300

    add lambda to terraform

--- infra/main.tf
+++ infra/main.tf
@@ -36,3 +36,52 @@ resource "aws_s3_bucket_public_access_block" "fast_api_lambda_bucket" {
   ignore_public_acls      = true
   restrict_public_buckets = true
 }
+
+resource "aws_iam_role" "fast_api_lambda_exec" {
+  name = "fast-api-lambda"
+
+  assume_role_policy = <<POLICY
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Principal": {
+        "Service": "lambda.amazonaws.com"
+      },
+      "Action": "sts:AssumeRole"
+    }
+  ]
+}
+POLICY
+}
+
+resource "aws_iam_role_policy_attachment" "fast_api_lambda_policy" {
+  role       = aws_iam_role.fast_api_lambda_exec.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
+}
+
+resource "aws_lambda_function" "fast_api_lambda" {
+  function_name = "fast-api-lambda"
+
+  s3_bucket = aws_s3_bucket.fast_api_lambda_bucket.id
+  s3_key    = aws_s3_object.fast_api_lambda.key
+
+  runtime = "python3.10"
+  handler = "app.main.handler"
+
+  //source_code_hash = filebase64sha256("../function.zip")
+
+  role = aws_iam_role.fast_api_lambda_exec.arn
+
+}
+
+
+resource "aws_s3_object" "fast_api_lambda" {
+  bucket = aws_s3_bucket.fast_api_lambda_bucket.id
+
+  key    = "function.zip"
+  source = "../function.zip"
+
+  etag = filemd5("../function.zip")
+}
