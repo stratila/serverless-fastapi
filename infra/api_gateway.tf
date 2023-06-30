e049019da9c8f85daff28cf5dda720356e3fe864
Bohdan Stratila <googlobody@gmail.com>
2023-06-30 14:35:01 +0300

    add terraform api gateway

--- infra/api_gateway.tf
+++ infra/api_gateway.tf
@@ -72,7 +72,7 @@ resource "aws_api_gateway_integration" "api_integration" {
 
 }
 
-resource "aws_api_gateway_integraion_response" "api_integration_response" {
+resource "aws_api_gateway_integration_response" "api_integration_response" {
   for_each = local.responses
 
   rest_api_id = aws_api_gateway_integration.api_integration[each.value.method_key].rest_api_id
@@ -87,8 +87,8 @@ resource "aws_api_gateway_integraion_response" "api_integration_response" {
 resource "aws_api_gateway_method_response" "aws_api_gateway_method_response" {
   for_each = local.responses
 
-  rest_api_id = aws_api_gateway_response.api_response[each.key].rest_api_id
-  resource_id = aws_api_gateway_response.api_response[each.key].resource_id
+  rest_api_id = aws_api_gateway_integration_response.api_response[each.key].rest_api_id
+  resource_id = aws_api_gateway_integration_response.api_response[each.key].resource_id
   http_method = each.value.method
   status_code = each.value.status_code
 
@@ -105,7 +105,7 @@ resource "aws_api_gateway_deployment" "api_deployment" {
     aws_api_gateway_resource.api_resource,
     aws_api_gateway_method.api_method,
     aws_api_gateway_integration.api_integration,
-    aws_api_gateway_integraion_response.api_integration_response,
+    aws_api_gateway_integration_response.api_integration_response,
     aws_api_gateway_method_response.aws_api_gateway_method_response,
   ]
 
